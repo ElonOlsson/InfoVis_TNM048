ca6902353e8a70cc736ed384b57b636de0a95d18
ElonOlsson <olssonelon@gmail.com>
2018-02-08 17:12:59 +0100

    steg 4 fungerar, men inte done yet

--- lab2/Lab2/source/js/kmeans.js
+++ lab2/Lab2/source/js/kmeans.js
@@ -17,74 +17,80 @@ function kmeans(data, k) {
 	
     for(var i=0 ; i<k ; ++i){
         //centroidArray[i] = [ Math.random(), Math.random(), Math.random()]; // ska det vara slumpat i rymden, eller sättas till posisionen av ett slumpat dataelement?
-		
 		var c = Math.floor((Math.random() * data.length) + 1);
 		centroidArray.push(data[c]);
-		
+		console.log("what's inside centroidArray at position A: " + i + " : " + centroidArray[i].A);	//centroidArray[i][0]: undefined
     }
-
-    //2. Assign all items to the closest centroid with euclidiskt avstånd
 
     // K stycken dynamiska arrayer
     var assignedData = [];
 	var   cols = k;
 
-//init the grid matrix
-for ( var i = 0; i < cols; i++ ) {
-    assignedData[i] = []; 
-}
-	
-	
-	console.log("Size of k: "+assignedData.length);
-	
-	
-    for(var i=0 ; i < data.length; ++i){
-        var distance = 1000;
-		var closesedCentroidIndex = 0;
-		
-        for(var j=0 ; j<k ; ++j ){
-            var theSmallestDistance= Math.sqrt((centroidArray[j].A - data[i].A)^2 + (centroidArray[j].B - data[i].B)^2 + (centroidArray[j].C - data[i].C)^2);
-            
-			if (theSmallestDistance <= distance) {
-                distance = theSmallestDistance;
-                //peka på just den här centroiden;
-				closesedCentroidIndex = j;
-            }
-        }
-		assignedData[closesedCentroidIndex].push(i);
-    }
-    console.log(assignedData[0][0]);
-    console.log(assignedData[1][0]);
-    console.log(assignedData[2][0]);
-    console.log(assignedData[3][0]);
-
-    //3. recalculate the posision of the k centroids to be in the center of the cluster
-    //   This is achieved by calculating the average values in all dimensions.
-
-	for(var i = 0; i < k; ++i){
-		var AtotAverage = 0;
-		var BtotAverage = 0;
-		var CtotAverage = 0;
-		for(var j = 0; j < assignedData[i].length; ++j){
-			//average
-			AtotAverage += data[assignedData[i][j]].A;
-			BtotAverage += data[assignedData[i][j]].B;
-			CtotAverage += data[assignedData[i][j]].C;			
-		}
-		AtotAverage = AtotAverage/assignedData[i].length;
-		BtotAverage = BtotAverage/assignedData[i].length;
-		CtotAverage = CtotAverage/assignedData[i].length;
-		//assign new posision
-		centroidArray[i][0] = AtotAverage;
-		centroidArray[i][1] = BtotAverage;
-		centroidArray[i][2] = CtotAverage;
-		
+	//init the grid matrix
+	for ( var i = 0; i < cols; i++ ) {
+		assignedData[i] = [];
 	}
-	
-	//4. Check the quality of the cluster. Use the sum of the squared distance within each cluster as your measure of quality.
-    //  räkna ut summan.
+	console.log("Size of k: " + k +". Size of assignedData: "+assignedData.length);
 
-	
+    //2. Assign all items to the closest centroid with euclidiskt avstånd
+	while(true) {
+		for (var i = 0; i < data.length; ++i) {
+			var distance = 1000;
+			var closesedCentroidIndex = 0;
+
+			for (var j = 0; j < k; ++j) {
+				var theSmallestDistance = Math.sqrt((centroidArray[j].A - data[i].A) ^ 2 + (centroidArray[j].B - data[i].B) ^ 2 + (centroidArray[j].C - data[i].C) ^ 2);
+				if (theSmallestDistance <= distance) {
+					distance = theSmallestDistance;
+					//peka på just den här centroiden;
+					closesedCentroidIndex = j;
+				}
+			}
+			assignedData[closesedCentroidIndex].push(i);	//pusha in dataelement istället för int
+		}
+		console.log(assignedData[0][0]);
+		console.log(assignedData[1][0]);
+		console.log(assignedData[2][0]);
+		console.log(assignedData[3][0]);
+
+		//3. recalculate the posision of the k centroids to be in the center of the cluster
+		//   This is achieved by calculating the average values in all dimensions.
+
+		for (var i = 0; i < k; ++i) {
+			var AtotAverage = 0;
+			var BtotAverage = 0;
+			var CtotAverage = 0;
+			for (var j = 0; j < assignedData[i].length; ++j) {
+				//average
+				AtotAverage += data[assignedData[i][j]].A;
+				BtotAverage += data[assignedData[i][j]].B;
+				CtotAverage += data[assignedData[i][j]].C;
+			}
+			AtotAverage = AtotAverage / assignedData[i].length;
+			BtotAverage = BtotAverage / assignedData[i].length;
+			CtotAverage = CtotAverage / assignedData[i].length;
+			//assign new posision
+			centroidArray[i].A = AtotAverage;
+			centroidArray[i].B = BtotAverage;
+			centroidArray[i].C = CtotAverage;
+		}
+
+		//4. Check the quality of the cluster. Use the sum of the squared distance within each cluster as your measure of quality.
+		//  räkna ut summan.
+		//var theSmallestDistance= Math.sqrt((centroidArray[j].A - data[i].A)^2 + (centroidArray[j].B - data[i].B)^2 + (centroidArray[j].C - data[i].C)^2);
+		var totSum = 0;
+		for (var i = 0; i < k; ++i) {	//för varje kluster
+			var sumSquaredDistance = 0;
+			for (var j = 0; j < assignedData[i].length; ++j) {	//och klustrets alla tillhörande dataelement
+				sumSquaredDistance += (data[assignedData[i][j]].A - centroidArray[i].A) ^ 2 + (data[assignedData[i][j]].B - centroidArray[i].B) ^ 2 + (data[assignedData[i][j]].C - centroidArray[i].C) ^ 2;					//dataelement - centroid
+			}
+			totSum += sumSquaredDistance;			//detta sumsystemet var mest bara för att testa och se hur mycket felet blir
+			console.log("totSum: " + totSum);
+		}
+		if (totSum > 1) {	//ska vara < än ett rimligt tröskelvärde.
+			return assignedData;
+		}
+	}
 
 
 	//	jämför med tröskelvärde
@@ -95,26 +101,7 @@ for ( var i = 0; i < cols; i++ ) {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
